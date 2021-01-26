dc8c48b23568eb65eeb2281609185b19dc1c19f4
DimArgyros <78039507+DimArgyros@users.noreply.github.com>
2021-01-26 18:40:43 +0000

    Update PS1_code.R

--- PS1_code.R
+++ PS1_code.R
@@ -4,8 +4,8 @@ tb <- tibble(
   female = ifelse(runif(10000)>=0.50,1,0),
   ability = rnorm(10000),
   discrimination = female,
-  occupation = 9 + round(1 + 2*ability + 0*female - 2*discrimination + rnorm(10000)),
-  wage = 1 - 1*discrimination + 1*occupation + 2*ability + rnorm(10000) 
+  occupation = 9 + round(1 + 2*ability + 0*female - 2*discrimination + rnorm(10000)), %% We added the number 9, in order for the dummy to take the values 0-9 with 0 meaning unemployed & 9 top tier occupation statues e.g. managerial%%
+  wage = 1 - 1*discrimination + 1*occupation + 2*ability + rnorm(10000)  %% The constants -1, 1, 2 etc are derived from the direction of the arrows & paths. For example ability affects earnings through two paths (directly & throuhh occupation) so it is multiplied by 2%%
 )
 
 lm_1 <- lm(wage ~ discrimination, tb)
